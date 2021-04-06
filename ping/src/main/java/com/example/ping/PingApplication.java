5e627073ef08774077e15b037c146b362e7a8072
Kenny Hung <kennysubs+github@outlook.com>
2021-04-06 17:45:45 +0100

    Greet test, solution

--- ping/src/main/java/com/example/ping/PingApplication.java
+++ ping/src/main/java/com/example/ping/PingApplication.java
@@ -2,20 +2,29 @@ package com.example.ping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 public class PingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PingApplication.class, args);
+    }
 
-	@GetMapping("/ping")
-	public String sayHello(@RequestParam(value = "", defaultValue = "") String name) {
-		return "pong";
-	}
+    @GetMapping("/ping")
+    public String sayHello(@RequestParam(value = "", defaultValue = "") String name) {
+        return "pong";
+    }
+
+    @PostMapping("/greet")
+    public String greet(@RequestBody GreetRequest greetRequest) {
+        return "Hello " + greetRequest.name;
+    }
+
+    public static class GreetRequest {
+        // TODO: Learn about immutable objects
+        // TODO: Learn about Lombok
+        public String name;
+    }
 }
