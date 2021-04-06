70c1e2fb4fc505b6e53138757325c14ec5b5db71
Kenny Hung <kenny@kenny-linux.ken.net>
2021-04-06 16:46:04 +0100

    First tests

--- ping/src/test/java/com/example/ping/PingApplicationTests.java
+++ ping/src/test/java/com/example/ping/PingApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.ping;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
+@AutoConfigureMockMvc
+@WebMvcTest
 class PingApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	void canPingService() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/ping");
+		MvcResult result = mockMvc.perform(request).andReturn();
+
+		System.out.println(result);
 	}
 
 }
